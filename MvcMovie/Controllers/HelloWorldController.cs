c8d9c55cf9ccd72807f9fd4c1a60f13d7816be7a
karinodd <kucherenko-karina@inbox.ru>
2020-06-05 21:11:16 +0300

    Дополнено действие Welcome в HelloWorldController

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
@@ -9,9 +10,16 @@ namespace MvcMovie.Controllers
             return "Это моё стандартное действие...";
         }
 
-        public string Welcome()
+        //public string Welcome()
+        //{
+        //    return "Это действие при открытии Welcome...";
+        //}
+
+        public string Welcome(string name, int numTimes = -1)
         {
-            return "Это действие при открытии Welcome...";
+            if (string.IsNullOrEmpty(name) || numTimes == -1)
+                return "Необходимо ввести параметры";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes: {numTimes}");
         }
     }
 }
