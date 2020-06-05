a5cb9ab945c9c7a3ffddff8d046b6b7b4fb778dd
karinodd <kucherenko-karina@inbox.ru>
2020-06-05 20:58:32 +0300

    Добавлен контроллер HelloWorld

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Это моё стандартное действие...";
+        }
+
+        public string Welcome()
+        {
+            return "Это действие при открытии Welcome...";
+        }
+    }
+}
